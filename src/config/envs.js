f4e81bf7320b6e1eaadf4dcaeb78f05a62a95ffe
Adrian_B <burdilesricardo407@gmail.com>
2025-06-30 02:28:01 -0300

    seed con basedatos e info

--- src/config/envs.js
+++ src/config/envs.js
@@ -9,8 +9,9 @@ const envsSchema = joi.object({
     DB_HOST: joi.string().required(),
     DB_USER: joi.string().required(),
     // DB_PASSWORD: joi.string().required(),
-    DATABASE: joi.string().required(),
+    // DATABASE: joi.string().required(),
   })
+
   .unknown(true); // que me traiga las variables desconocidas..IMPORTANTE
 
   //!proceso de validacion de las variables de entorno
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js'; //importamos app para crear el servidor
 import { testConnection } from './db/db.js';
-import { runSeed } from '../seeds/seed.js'; // Importamos la función runSeed
+import { runSeed2 } from '../seeds/seed2.js'; // Importamos la función runSeed
+
 
 const main = () => {
     const port = app.get('port');
@@ -8,7 +9,8 @@ const main = () => {
     app.listen(port, async () =>{
         console.log('server listening on port', port);
         await testConnection (); //!esta funcion testea la conexion a la DB. IMPORTANTISIMA 
-        // await runSeed ();
+        
+        runSeed2 ();//inicia el seed 
         
     });
 }
